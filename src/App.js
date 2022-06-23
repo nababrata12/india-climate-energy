030b60ca89c5965d858b8a574d8307bfd4d8e3bd
Nababrata Deb <79349182+nababrata12@users.noreply.github.com>
2022-06-22 17:56:55 -1200

    energy

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = () => {
 
               <Routes>
                 {/* dashboard  */}
-                <Route path="/overview" element={<OverView />} />{/* pages  */}
+                <Route path="/energy" element={<OverView />} />{/* pages  */}
 
                 {/* apps  */}
 
