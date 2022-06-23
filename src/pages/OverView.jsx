c7177d964e6c08f690254ed069178199f11cf362
Nababrata Deb <79349182+nababrata12@users.noreply.github.com>
2022-06-23 06:11:44 -1200

    updatetee

--- src/pages/OverView.jsx
+++ src/pages/OverView.jsx
@@ -10,7 +10,7 @@ import React from 'react';
 
 function OverView() {
   return (
-   <div className="mt-24">
+    <div className="mt-24">
       {/* <script type='text/javascript' src='https://prod-apnortheast-a.online.tableau.com/javascripts/api/viz_v1.js'></script><div className='tableauPlaceholder' style={{ width: 1536, height: 627 }}><object class='tableauViz' width='1536' height='627' style='display:none;'><param name='host_url' value='https%3A%2F%2Fprod-apnortheast-a.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;indianenergyandclimatedashboardusingmessageix' /><param name='name' value='rajkhush&#47;ProductUtilization' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /></object></div> */}
       <iframe
         className='sm:w-screen lg:w-screen h-screen'
@@ -23,5 +23,3 @@ function OverView() {
 }
 
 export default OverView;
-
-// width: 1536px; height: 627px;
