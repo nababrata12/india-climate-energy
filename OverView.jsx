5378364bf49ae7df5b30d6ee8b6094b0c43a2a8b
Nababrata Deb <79349182+nababrata12@users.noreply.github.com>
2022-06-23 10:12:55 +0530

    add file via upload

---
+++ OverView.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable react/self-closing-comp */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable jsx-quotes */
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable jsx-a11y/iframe-has-title */
+/* eslint-disable react/style-prop-object */
+/* eslint-disable max-len */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+function OverView() {
+  return (
+    <div className="mt-24">
+      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
+          <div className="flex justify-between items-center">
+            <div>
+              <script type='text/javascript' src='https://prod-apnortheast-a.online.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style={{ width: 1536, height: 627 }}><object class='tableauViz' width='1536' height='627' style='display:none;'><param name='host_url' value='https%3A%2F%2Fprod-apnortheast-a.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;indianenergyandclimatedashboardusingmessageix' /><param name='name' value='rajkhush&#47;ProductUtilization' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /></object></div>
+              {/* <iframe src="https://prod-apnortheast-a.online.tableau.com/t/indianenergyandclimatedashboardusingmessageix/views/rajkhush/ProductUtilization?:origin=card_share_link&:embed=n" frameBorder="0" /> */}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default OverView;
+
+// width: 1536px; height: 627px;
