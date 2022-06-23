542ab2108f2e88bbfde976686e86ae02c1ba9eb8
Nababrata Deb <79349182+nababrata12@users.noreply.github.com>
2022-06-23 06:10:06 -1200

    push11

--- src/pages/OverView.jsx
+++ src/pages/OverView.jsx
@@ -10,17 +10,14 @@ import React from 'react';
 
 function OverView() {
   return (
-    <div className="mt-24">
-      <div className="flex flex-wrap lg:flex-nowrap justify-center ">
-        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg h-44 rounded-xl w-full lg:w-80 p-8 pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center">
-          <div className="flex justify-between items-center">
-            <div>
-              <div className='tableauPlaceholder' id='viz1655962514815' style={{ position: 'relative' }}><noscript><a href='#'><img alt=' ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;El&#47;ElectricityGeneration_0&#47;Source&#47;1_rss.png' style={{ border: 'none' }} /></a></noscript><object className='tableauViz' style={{ display: 'none' }}><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='ElectricityGeneration_0&#47;Source' /><param name='tabs' value='yes' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;El&#47;ElectricityGeneration_0&#47;Source&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1655962514815');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1000px';vizElement.style.height='850px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
-              {/* <iframe src="https://prod-apnortheast-a.online.tableau.com/t/indianenergyandclimatedashboardusingmessageix/views/rajkhush/ProductUtilization?:origin=card_share_link&:embed=n" frameBorder="0" /> */}
-            </div>
-          </div>
-        </div>
-      </div>
+   <div className="mt-24">
+      {/* <script type='text/javascript' src='https://prod-apnortheast-a.online.tableau.com/javascripts/api/viz_v1.js'></script><div className='tableauPlaceholder' style={{ width: 1536, height: 627 }}><object class='tableauViz' width='1536' height='627' style='display:none;'><param name='host_url' value='https%3A%2F%2Fprod-apnortheast-a.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;indianenergyandclimatedashboardusingmessageix' /><param name='name' value='rajkhush&#47;ProductUtilization' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /></object></div> */}
+      <iframe
+        className='sm:w-screen lg:w-screen h-screen'
+        src="https://public.tableau.com/views/energyindia-Final_1/PowerGenerationConsumption?:language=en-US&:display_count=n&:origin=viz_share_link"
+        frameBorder="0"
+      />
+      {/* <div className='tableauPlaceholder' id='viz1655964122670' style={{ position: 'relative' }}><noscript><a href='#'><img alt='What are you going to save for future generation? Power? Can you infer why? ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;en&#47;energyindia-Final_1&#47;PowerGenerationConsumption&#47;1_rss.png' style={{ border: 'none' }} /></a></noscript><object className='tableauViz' style={{ display: 'none' }}><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='energyindia-Final_1&#47;PowerGenerationConsumption' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;en&#47;energyindia-Final_1&#47;PowerGenerationConsumption&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1655964122670');                    var vizElement = divElement.getElementsByTagName('object')[0];                    vizElement.style.width='1200px';vizElement.style.height='827px';                    var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script> */}
     </div>
   );
 }
